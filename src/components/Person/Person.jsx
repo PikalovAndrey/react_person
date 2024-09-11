0d55108902a0b000777b2a5214b47ca8fc1f289f
PikalovAndrey <andrtan10mail.com@gmail.com>
2024-09-11 10:16:37 +0300

    improve the code

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,22 +1,17 @@
 export const Person = ({ person }) => {
-    let partnerText;
-  
-    if (person.isMarried) {
-      partnerText = person.sex === 'f'
+  let partnerText = person.isMarried
+    ? (person.sex === 'f'
         ? `${person.partnerName} is my husband`
-        : `${person.partnerName} is my wife`;
-    } else {
-      partnerText = 'I am not married';
-    }
-  
-    return (
-      <section className="Person">
-        <h2 className="Person__name">My name is {person.name}</h2>
-        
-        {person.age && <p className="Person__age">I am {person.age}</p>}
-  
-        <p className="Person__partner">{partnerText}</p>
-      </section>
-    );
-  };
-  
+        : `${person.partnerName} is my wife`) 
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+
+      {person.age && <p className="Person__age">I am {person.age}</p>}
+
+      <p className="Person__partner">{partnerText}</p>
+    </section>
+  );
+};
